8c33bd845d9a0693b46e337dbc1666182d779fa7
pvictorf <pvictorferreira@hotmail.com>
2022-03-22 10:49:00 -0300

    Order Json

--- assets/js/podium.js
+++ assets/js/podium.js
@@ -7,16 +7,10 @@ export async function fetchPodium() {
   return json;
 }
 
-function orderPodium(podium) {
-  let order = []
-  podium.forEach(p => {
-    if(p.position % 2 === 0) order.push(p);
-    else order.unshift(p)
-  })
-  return order;
-}
 
-export function draw(data) {
+export function draw(json) {
+  const data = json.sort((a, b) => a.position - b.position)
+  
   const podiumRender = $$('#podium-render')
   const rankingRender = $$('#ranking-render')
 
@@ -27,6 +21,24 @@ export function draw(data) {
   rankingRender.innerHTML = ranking.map(r => rankingTemplate(r)).join('');
 }
 
+function orderPodium(podium) {
+  let order = []
+  podium.forEach(p => {
+    if(p.position % 2 === 0) order.push(p);
+    else order.unshift(p)
+  })
+  return order;
+}
+
+function suffixNth(d) {
+  if (d > 3 && d < 21) return 'th';
+  switch (d % 10) {
+      case 1:  return "st";
+      case 2:  return "nd";
+      case 3:  return "rd";
+      default: return "th";
+  }
+};
 
 function podiumTemplate(podium) {
   const barSize = {
@@ -51,7 +63,7 @@ function podiumTemplate(podium) {
 function rankingTemplate(raking) {
   return `
   <div class="ranking fade-in">
-    <p class="ranking_position">${raking.position}</p>
+    <p class="ranking_position">${raking.position}°</p>
     <img class="ranking_avatar" src="${raking.avatar}" onerror="this.onerror=null;this.src='./assets/images/no-image.svg'"/>
     <div class="raking_info">
       <p class="ranking_name">${raking.name}</p>
